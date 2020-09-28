dd6353609ec1e94d58425cefef6191834ce7d682
winplaybox <sagarasuthar530@gmail.com>
2020-09-28 18:17:03 +0530

    Updates

---
+++ precache-manifest.61a019b0645984365c8792ca093f60e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c27ede362e3cfe383da070af814724",
+    "url": "/Winplaybox/golive.git/index.html"
+  },
+  {
+    "revision": "26edbbcf793adf0db503",
+    "url": "/Winplaybox/golive.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3d435c9b6af769958be3",
+    "url": "/Winplaybox/golive.git/static/js/2.71327c48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Winplaybox/golive.git/static/js/2.71327c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26edbbcf793adf0db503",
+    "url": "/Winplaybox/golive.git/static/js/main.72a07984.chunk.js"
+  },
+  {
+    "revision": "0bfb4f14444c5ba9e23a",
+    "url": "/Winplaybox/golive.git/static/js/runtime-main.02bb9512.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Winplaybox/golive.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
